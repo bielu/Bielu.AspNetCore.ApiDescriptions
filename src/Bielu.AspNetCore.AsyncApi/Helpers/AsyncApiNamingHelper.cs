6f695b40d3bd571ac012a29950b299bd78ff0da1
Arkadiusz biel <bielu@bielu.com.pl>
2026-02-24 19:53:13 +0000

    add helper

---
+++ src/Bielu.AspNetCore.AsyncApi/Helpers/AsyncApiNamingHelper.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Bielu.AspNetCore.AsyncApi.Helpers;
+
+internal static class AsyncApiNamingHelper
+{
+    public static string SanitizeKey(string key)
+    {
+        if (string.IsNullOrWhiteSpace(key)) return key;
+        return Regex.Replace(key, @"[^a-zA-Z0-9\.\-_]", string.Empty);
+    }
+}
